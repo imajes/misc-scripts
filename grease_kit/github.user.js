52871e9795625a4dbd9131350c11016be5f33538
James Cox <james@imaj.es>
2011-09-25 19:58:46 -0400

    store the github userjs

---
+++ grease_kit/github.user.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name           github code
+// @description    niceify github's pre blocks
+// @include        https://github.com/*
+// ==/UserScript==
+var style;
+
+function enableStyle() {
+  var heads = document.getElementsByTagName("head");
+  if (heads.length > 0) {
+    var node = document.createElement("style");
+    node.type = "text/css";
+    node.appendChild(document.createTextNode("pre { font-family: 'inconsolata-dz' !important; }"));
+    heads[0].appendChild(node);
+  }
+}
+
+document.addEventListener('keypress', function (event) {
+  if (event.keyCode == 18) {
+    if (style == null) {
+      enableStyle();
+    } else {
+      document.getElementsByTagName('head')[0].removeChild(style);
+      style = null;
+    }
+    event.stopPropagation();
+    event.preventDefault();
+  }
+}, true);
+
+enableStyle();
+
